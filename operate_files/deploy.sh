4cb1a2a789eefd6a799f8ab3ca77dede5b16814c
Teja Vemagiri <tejavemagiri5@gmail.com>
2024-08-12 21:11:31 +0200

    final

--- operate_files/deploy.sh
+++ operate_files/deploy.sh
@@ -5,24 +5,34 @@
 # $2: tag
 # $3: ssh_key
 
-. $1
-
 SECURITY_GROUP_NAME="security_$2"
 NETWORK_NAME="network_$2"
 KEY_NAME="$3"
-VM_NAME="vm4_$2"
+
+# Function to get flavor ID based on criteria
+get_flavor_id() {
+    local FLAVOR_NAME=$(openstack flavor list -c ID -c RAM -c Disk -c VCPUs -f csv | awk -F ',' '$2 == 2048 && $3 == 20 && $4 == 2 {print $1}' | sed 's/"//g' | head -n 1)
+    
+    if [ -z "$FLAVOR_NAME" ]; then
+        echo "No suitable flavor found."
+        exit 1
+    fi
+
+    echo $FLAVOR_NAME
+}
 
 create_vm() {
     local VM_NAME=$1
     local TAG=$2
     local KEY_NAME=$3
+    local FLAVOR_NAME=$4
     
     echo "Key Name is: $KEY_NAME"
     echo "Creating VM: $VM_NAME"
     
     # Create the VM and capture its ID
     vm_id=$(openstack server create \
-        --flavor "2C-2GB-20GB" \
+        --flavor "$FLAVOR_NAME" \
         --image "Ubuntu 20.04 Focal Fossa x86_64" \
         --security-group $SECURITY_GROUP_NAME \
         --key-name "$KEY_NAME" \
@@ -38,7 +48,7 @@ create_vm() {
         if [ "$status" = "ACTIVE" ]; then
             break
         fi
-        sleep 20 # Wait for 30 seconds before checking again
+        sleep 20 # Wait for 20 seconds before checking again
     done
     
     # Get the VM's IP address
@@ -52,4 +62,12 @@ create_vm() {
     fi
 }
 
-create_vm "vm4_$2" "$2" "$3"
+# Get the appropriate flavor name
+FLAVOR_NAME=$(get_flavor_id)
+
+# Generate a 6-digit Unix time
+timestamp=$(date +%s | tail -c3)
+
+# VM name with 6-digit Unix time
+VM_NAME="vm${timestamp}_$2"
+create_vm "$VM_NAME" "$2" "$3" "$FLAVOR_NAME"
